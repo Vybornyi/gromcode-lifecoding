e030a9fdaaa987e0af72c7fed1a7d63cb8366909
Vybornyi <1vybornyi@gmail.com>
2022-07-06 13:26:27 +0300

    add

--- js/3-array/index.js
+++ js/3-array/index.js
@@ -1,101 +1,58 @@
-/* ==================================== */
-/* ===> мутирующие методы массивов <=== */
-/* ===> изменяют исходный массив <===== */
-/* ==================================== */
+const numbersList = [5, 0, 8, 10, -4, 50, 220];
+console.log(numbersList);
+//input: callback
+//output: array
 
-/* pop - удалит из массива последний элемент и вернет его */
-
-// c помощью метода pop достань последний элемент массива numbersList1 и помести его в переменную lastNumber
-// выведи в консоль эту переменную, а так же массив numbersList1 после выполнения pop
-
-const numbersList1 = [1, 2, 3, 4, 5];
-const lastEl = numbersList1.pop();
-console.log(lastEl);
-console.log(numbersList1);
+//input: element ,index(optional), array(optional)
+//output: new element
 
 
-// put your code here
-
-/* push - добавит элемент в конец массива */
-
-// c помощью метода push добавь число 6 в конец массива numbersList2
-// выведи в консоль результат работы метода push, а так же массив numbersList2 после выполнения push
-
-const numbersList2 = [1, 2, 3, 4, 5];
-console.log('BEFORE PUSH',numbersList2);
-const pushNumber = numbersList2.push(777);
-console.log(pushNumber);
-console.log('AFTER PUSH',numbersList2);
-
-// put your code here
-
-/* shift - удалит из массива первый элемент и вернет его */
-
-// c помощью метода shift достань первый элемент массива numbersList3 и помести его в переменную firstNumber
-// выведи в консоль эту переменную, а так же массив numbersList2 после выполнения shift
-
-const numbersList3 = [1, 2, 3, 4, 5];
-
-console.log('BEFORE SHIFT', numbersList3);
-const shiftNumber = numbersList3.shift();
-console.log(shiftNumber);
-console.log('AFTER SHIFT', numbersList3);
-
-
-// put your code here
-
-/* unshift - добавит элемент в начало массива */
-
-// c помощью метода unshift добавь число 0 в начало массива
-// выведи в консоль результат работы метода unshift, а так же массив numbersList4 после выполнения unshift
-
-const numbersList4 = [1, 2, 3, 4, 5];
-
-// put your code here
-
-/* ======================================= */
-/* ===> НЕ мутирующие методы массивов <=== */
-/* ===> НЕ изменяют исходный массив <===== */
-/* ======================================= */
-
-// начальный массив
-//input:function
-//output: new array
-const anotherNumbersList = [5, 0, 8, 10, -4, 50, 220];
-const filterResult = anotherNumbersList.filter(function filterCallback(el) {
-    return el > 5;
+/* const mapResV1 = numbersList.map(el => {
+  let newEl = null;
+  if (index > 2) {
+    newEl = el * el;
+  } else {
+    newEl = el;
   }
-);
-anotherNumbersList.filter(el => el > 5);
+  return newEl;
+}); */
+
+const mapResV2 = numbersList.map((el, index) => index > 2 ? el * el : el);
+console.log(mapResV2);
+
+const mapResV3 = numbersList.map(item => item * item);
+console.log(mapResV3);
+
+//forEach
+//input: requestIdleCallback;
+//output: none
+
 //callback
-//input: number
-//output: boolean
-
-/* метод filter */
-/* const newArr = arr.filter(callback) - создает и возвращает новый отфильтрованный массив.
-   ф-ция callback запустится по очереди для каждого элемента начального массива arr
-   в newArr попадут только те элементы arr, для которых callback(arr[i]) вернет true
-   callback(arr[i]) === true -> элемент будет добавлен в финальный массив
-   callback(arr[i]) === false -> элемент НЕ будет добавлен в финальный массив */
-
-// c помощью метода filter создай новый массив bigNumbers, в котором будут числа > 5 из anotherNumbersList
-// выведи bigNumbers в консоль
-
-// put your code here
-
-// c помощью метода filter создай новый массив evenPositions, в котором будут числа из четных позиций которые > 5
-// массива anotherNumbersList
-// выведи evenPositions в консоль
-
-// put your code here
- function sum(from,to,resolver){
-  let sumRes = 0;
-  for (let index = from; index <=to; index +=1){
-    sumRes += index;
+//input: element ,index(optional), array(optional)
+//output: undefined
+/* const forEachRes = numbersList.forEach(el => {
+  if (el > 0) {
+    console.log(el);
   }
-  resolver(sumRes);
- }
- const resolver =  (input) =>{
-   return console.log(input);
- };
- sum(2,4,resolver);
+}); */
+
+//reduse
+//input:callback,acc init value(optional)
+//output:number
+
+//callback:acc,element.index(opt),array(opt)
+//input:new acc
+/* const transactions = [5, 11, 8, 10, -4, 50, 220, 1203, 556, 41];
+const redusseSum = transactions.reduce((sum, el) => {
+  console.log('sum', sum);
+  console.log('el', el);
+  if (el > 100) {
+    return sum + el;
+  }
+  return sum;
+}, 0
+); */
+const transactions = [5, 11, 8, 10, -4, 50, 220, 1203, 556, 41];
+const redusseSum = transactions.reduce((sum, el) =>
+  (el > 100 ? sum + el : sum), 0);
+console.log(redusseSum);  
